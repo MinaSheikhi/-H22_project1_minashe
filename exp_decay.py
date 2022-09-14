a1433fa8b1dc553358f342b16486364deffac892
minashe <minashe@math.uio.no>
2022-09-14 17:25:39 +0200

    La til oppg 1c, til 1g

--- exp_decay.py
+++ exp_decay.py
@@ -1,16 +1,16 @@
 import numpy as np
+from scipy.integrate import solve_ivp
+from ode import ODEModel
 
-# Oppgave 1a
-
-class ExponentialDecay:
+class ExponentialDecay(ODEModel):
     def __init__(self, a: float):
         if (a < 0):
             raise ValueError ("a should be a positive integer")
         self.a = a
 
     def __call__(self, t: float, u: np.ndarray) -> np.ndarray:
-        return - self.a * u
-       
+        f = lambda t, u: -self.a * u
+        return f(t,u)
 
     @property     # built in spesial function that creates and returns a property object.
     def decay(self): # Turning a into a property
@@ -21,6 +21,31 @@ class ExponentialDecay:
         if (x < 0):
             raise ValueError ("Value is not positive.")
         self._a = x
+
+    def num_states(self) -> int:
+        return 1
+
+def solve_exponential_decay():
+    t = 0.3
+    model = ExponentialDecay(0.4)
+    u = np.array([3.2])
+    du_dt = model.__call__(t,u)
+    # array with routines based on numerical ranges
+    sol = solve_ivp(du_dt, t_eval = np.arange(0, 10, 0.1) , t_span = (0,10) , y0 = (0,))
+    return sol
+
+
+
+
+
+
+
+        
+        
+
+
+
+
 
 
 
--- test_exp_decay.py
+++ test_exp_decay.py
@@ -1,11 +1,15 @@
+from re import T
 from exp_decay import ExponentialDecay
+from ode import solve_ode, InvalidInitialConditionError
 import math
 import numpy as np
 import pytest
 
+
+model = ExponentialDecay(0.4)
+
 def test_ExponentialDecay():
     t = 0.0
-    model = ExponentialDecay(0.4)
     u = np.array([3.2])
     du_dt = model(t,u)
     assert math.isclose(du_dt[0],-1.28)
@@ -20,5 +24,17 @@ def test_negative_decay_raises_ValueError_1b():
     with pytest.raises(ValueError):
         model.decay = -1.0  # cant set a to new value -1 because of valueerror
               
+def test_num_states():
+    model = ExponentialDecay(0.4)
+    model.num_states == 1
+
+def test_solve_time():
+    t0 = 0
+    T = 10
+    dt = 0.2
+
+def test_solve_with_different_number_of_initial_states():
+    with pytest.raises(InvalidInitialConditionError):
+        solve_ode(model, u0 = [0,1], T = 10.0, dt = 0.2)
 
 
