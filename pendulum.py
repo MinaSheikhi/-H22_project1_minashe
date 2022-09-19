c8e5f551ba631a3bda7c0a8823a0bb1b66e27a78
minashe <minashe@math.uio.no>
2022-09-19 16:47:33 +0200

    til oppgave 2g

--- pendulum.py
+++ pendulum.py
@@ -1,5 +1,7 @@
 import numpy as np
-from ode import ODEModel, solve_ode, plot_ode_solution
+import matplotlib.pyplot as plt
+from dataclasses import dataclass
+from ode import ODEModel, solve_ode, plot_ode_solution, ODEResult
 
 
 class Pendulum(ODEModel):
@@ -9,10 +11,10 @@ class Pendulum(ODEModel):
         self.g = g
 
     def __call__(self, theta: float, omega: float):
-        dOmega_dt = lambda t: - self.g * np.sin(theta) / self.L
-        dtheta_dt = lambda t: omega
+        dOmega_dt = (-self.g/self.L) * np.sin(theta)
+        dtheta_dt = omega
          
-        return dtheta_dt
+        return np.array([dtheta_dt, dOmega_dt])
 
     @property   
     def num_states(self):
@@ -20,18 +22,113 @@ class Pendulum(ODEModel):
 
 
 def exercise_2b(
-    u0: np.ndarray = [np.pi/6, 0.35],
-    T: float = 10.0,
-    dt:float = 0.01,
-):
+    u0: np.ndarray,
+    T: float,
+    dt:float,
+) -> ODEResult:
     model = Pendulum()
     result = solve_ode(model, u0, T, dt)
+    return result
 
+
+
+
+@dataclass #makes the class store data
+class PendulumResults:
+    results: ODEResult
+    pendulum: Pendulum
+
+
+    @property #this extracts the correct arrays from ode results
+    def theta(self) -> np.ndarray:
+        return self.results.solution[0]
+        
+
+    @property
+    def omega(self) -> np.ndarray:
+        return self.results.solution[1]
+        
+    @property
+    def x(self) -> np.ndarray:
+        return self.pendulum.L * np.sin(self.theta)
+
+    @property
+    def y(self) -> np.ndarray:
+        return - self.pendulum.L * np.cos(self.theta)
+
+    @property
+    def potential_energy(self, t) -> np.ndarray:  #hva slags t? self.result.time?
+        P = lambda t: self.pendulum.g * (self.y + self.pendulum.L)
+        return P
+    
+    @property
+    def vx(self) -> np.ndarray:
+        return np.gradient(self.x, self.results.time)
+
+    @property
+    def vy(self) -> np.ndarray:
+        return np.gradient(self.y, self.results.time)
+
+    @property
+    def kinetic_energy(self) -> np.ndarray:
+        return (1/2 * (self.vx**2 + self.vy**2))
+
+    @property
+    def total_energy(self):
+        return self.potential_energy + self.kinetic_energy
+
+
+def solve_pendulum(
+    u0: np.ndarray, T: float, dt: float = 0.01, pendulum: Pendulum = Pendulum()
+    ) -> PendulumResults:
+    return solve_ode(u0 = u0, T = T, dt = dt, model = pendulum)
+
+
+def plot_energy(results: PendulumResults, filename: str = None) -> None:
+    plt.plot(results.time, results.potential_energy, label = "Potential Energy")
+    plt.plot(results.time, results.kinetic_energy, label = "Kinetic energy")
+
+
+    plt.legend()
+
+    plt.grid(True)
+
+    plt.show()
+
+    plt.savefig(filename)
+
+"""
+def exercise_2g():
+    u0 = np.array(np.pi, 0.35)
+    T = 10.0
+    dt = 0.01
+    
+    sol = solve_pendulum(u0 = u0, T = T, dt = dt)
+    return plt.energy(sol, filename = "energy_single.png")
+
+
+"Yes the energy is conserved. This means that the total energy is always constant."
+
+
+class DampenedPendulum(Pendulum):
+    pass
+
+if __name__=="__main__":
+    #Exercise 2b
+    solution = exercise_2b([np.pi/6, 0.35], 10.0, 0.01)
+    print(solution)
     plot_ode_solution(results = result, state_labels = ["theta", "omega"], filename = "exercise_2b.png")
+    #Exercise 2g
+    exercise_2g()
+"""
+
+solution = exercise_2b(u0 = np.array([np.pi/6, 0.35]), T= 10.0, dt = 0.01)
+print(np.array(solution))
 
 
-if __name__ == "__main__":
-    exercise_2b([np.pi/6, 0.35], 10.0, 0.01)
+
+
+
 
 
 
--- test_pendulum.py
+++ test_pendulum.py
@@ -1,23 +1,42 @@
 import numpy as np
-from pendulum import Pendulum, exercise_2b
+import pytest
+from pendulum import Pendulum, exercise_2b, solve_pendulum
 
 
-"""@@pytest.mark.parametrize("L, gamma, theta, solution", [1.42, 0.35, np.pi/6, ], [1.42, 0, 0, 0])
-def test_Pendulum(L, gamma, theta):
-    model = Pendulum()
-    dTheta = model(theta, gamma)
-    solution =
-    assert """
+@pytest.mark.parametrize("L, theta, omega, dOmega", [
+    (1.42, np.pi/6, 0.35, (-9.81/1.42)*np.sin(np.pi/6)), 
+    (1.42, 0, 0, 0)])
+
+def test_Pendulum(L, omega, theta, dOmega):
+    p = Pendulum(L = L)
+    u = np.array([theta, omega])
+
+    assert p(0, u)[0] == omega and p(0,u)[1] == dOmega
+
+    """
+    derivatives = p(theta= theta, omega= omega)
+    theta_comp = derivatives[0]
+    omega_comp = derivatives[1]
+    
+    assert theta_comp == omega and omega_comp == dOmega
+"""
 
 def test_solve_pendulum_ode_with_zero_ic():
-    res = exercise_2b(u0 = np.array([0, 0]))
-
-    assert res.result.all() == 0
-
-
-
-
+    u0 = np.array([0, 0])
+    res = exercise_2b(u0, T = 10, dt = 0.01)
     
+    print(res.result)
+
+    assert all(res.result[0] == 0) and all(res.restult[1] ==  0)
+"""
+def test_solve_pendulum_function_zero_ic():
+    solution = solve_pendulum(u0 = np.array([0, 0]), T = 10, dt = 0.1)
+          
+    assert solution.theta.all() == 0 and solution.omega.all() == 0
+    assert solution.x.all == 0 and solution.y.all() == - solution.pendulum.L
+
+"""
+test_solve_pendulum_ode_with_zero_ic()  
 
 
 
