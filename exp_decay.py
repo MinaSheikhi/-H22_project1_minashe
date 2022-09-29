01604304836e21483d75fa55d505ba5bcbfdf399
Maha Riazi <mahari@uio.no>
2022-09-29 13:15:15 +0100

    Update exp_decay.py

--- exp_decay.py
+++ exp_decay.py
@@ -1,9 +1,11 @@
 import numpy as np
+import cmath
+from ode import ODEModel, ODEResult, plot_ode_solution, solve_ode
 from scipy.integrate import solve_ivp
-from ode import ODEModel
+
 
 class ExponentialDecay(ODEModel):
-    def __init__(self, a: float):
+    def __init__(self, a) -> None:
         """
         Constructor for ExponentianlDecay. 
 
@@ -14,9 +16,18 @@ class ExponentialDecay(ODEModel):
         Raises:
             ValueError: if input value is negative.
         """
+        self.decay = a
+    
+    @property
+    def decay(self) -> float:
+        return self._a
 
-        self.a = a
-
+    @decay.setter
+    def decay(self, value) -> None:
+        if value < 0:
+            raise ValueError("Constant cannot be negative.")
+        self._a = value
+    
     def __call__(self, t: float, u: np.ndarray) -> np.ndarray:
         """
         Call function that returns the derivative of u at time t. 
@@ -29,19 +40,10 @@ class ExponentialDecay(ODEModel):
             - a numpy array, the derivate dudt
         """
 
-        f = lambda t, u: -self.a * u
-        return f(t,u)
+        f = lambda t, u: -self.decay*u
+        return f(t, u)
 
-    @property     # built in spesial function that creates and returns a property object.
-    def decay(self): # Turning a into a property
-        return self._a
-
-    @decay.setter  #this makes the a variable private (local variable) with value x
-    def decay(self, x):
-        if (x < 0):
-            raise ValueError ("Value is not positive.")
-        self._a = x
-
+    @property
     def num_states(self) -> int:
         """
         Returns number of state variables in ODE.
@@ -51,38 +53,45 @@ class ExponentialDecay(ODEModel):
         """
 
         return 1
-
-def solve_exponentional_decay(
-    a: float = .4, 
-    u: np.ndarray = np.array([3.2]), 
-    u0: tuple = (0, ), 
-    T: int = 10, 
-    dt: float = .1, 
+    
+def solve_exponential_decay(
+    a: float, 
+    u: np.ndarray, 
+    u0: tuple, 
+    T: int, 
+    dt: float, 
 ) -> np.ndarray:
-
     """
     Instantiates the ExponentialDecay class and solves the ode using solve_ivp from scipy library.
-    Default solves for 
-        f(t = 0.4, u = [3.2]) with u0 = (0, ), T = 10, dt = 0.1
 
     Input
         - a: float
-            the decay constant
-        - u: np.ndarray
+            the exponential decay constant
+        - u: array
+            the u we are solving for
         - u0: tuple
-            initial condition for u
+            the initial condition for u
         - T: int
             end time
         - dt: float
-            timestep
-            
+            time step
+
     Output
         - an array
             Solved ode
     """
 
-    modell = ExponentialDecay(a)
+    model = ExponentialDecay(a)
     t = np.arange(0, T, dt)
-    sol = solve_ivp(fun=modell, t_span=(0,T), y0=u0, t_eval=t)
+    sol = solve_ivp(fun=model, t_span=(0,T), y0=u0, t_eval=t)
+
     return sol["t"], sol["y"]
 
+if __name__ == "__main__":
+    solve_exponential_decay(.4, np.array([3.2]), (0, ), 10, .01) # Exercise 1e
+
+    model = ExponentialDecay(0.4)
+    result = solve_ode(model, u0=np.array([4.0]), T=10.0, dt=0.01)
+    plot_ode_solution(
+        results=result, state_labels=["u"], filename="exponential_decay.png"
+    )
